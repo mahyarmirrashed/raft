1d2f377f10d9d202a39380e32ee5ea1bd715c86a
Mahyar Mirrashed <mah.mirr@gmail.com>
2022-06-09 20:04:42 -0500

    feat(rpc): add AddServer rpc schema
    Implement the AddServer RPC, as defined in Figure 4.1 of Diego Ongaro's Raft book using classes. A
    separate class is created for the request and response.

---
+++ src/rpc/add_server.py
@@ -0,0 +1,23 @@
+"""Defines the AddServer RPC (Remote Procedure Call) as per Figure 4.1."""
+
+from _base import BaseRPCRequest, BaseRPCResponse
+
+
+class AddServerRPCRequest(BaseRPCRequest):
+  """Implements AddServer RPC request arguments."""
+
+  @property
+  def new_server(self):
+    pass
+
+
+class AddServerRPCResponse(BaseRPCResponse):
+  """Implements AddServer RPC response results."""
+
+  @property
+  def status(self):
+    pass
+
+  @property
+  def leader_hint():
+    pass
