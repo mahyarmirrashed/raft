424e6777b78309a7d025d9cd8276406435aa49cc
Mahyar Mirrashed <mah.mirr@gmail.com>
2022-06-09 19:48:02 -0500

    feat(db): add schema for raft log
    Raft log will require an index/id, an associated term, the username of the individual being updated,
    the value to update inside the database. This serves as the basis for the key value store.

---
+++ src/db/schemas/raft.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS Entries (
+  id INTEGER PRIMARY KEY NOT NULL CHECK(id >= 0),
+  term INTEGER PRIMARY KEY NOT NULL CHECK(id >= 0),
+  username TEXT PRIMARY KEY NOT NULL CHECK (
+    LENGTH(username) <= 64
+    AND username NOT LIKE '%[^a-zA-Z0-9]%'
+  ),
+  balance INTEGER NOT NULL DEFAULT 0
+);
