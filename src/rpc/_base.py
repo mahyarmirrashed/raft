0044ba974955414d1929effde438e30bfdcdf2b2
Mahyar Mirrashed <mah.mirr@gmail.com>
2022-06-09 20:13:48 -0500

    fix(rpc): make validators private
    Field validators should not be public. Prefixing with underscores is Python notation for variables
    or methods that should not be accessing outside that class.

--- src/rpc/_base.py
+++ src/rpc/_base.py
@@ -11,7 +11,7 @@ class BaseRPC:
     pass
 
   @staticmethod
-  def validate_boolean(obj: Any, name: str = "object") -> None:
+  def _validate_boolean(obj: Any, name: str = "object") -> None:
     """Ensure object is boolean."""
     if not isinstance(name, str):
       raise TypeError("Object name must be a string.")
@@ -19,7 +19,7 @@ class BaseRPC:
       raise TypeError(f"{name.capitalize()} must be a boolean.")
 
   @staticmethod
-  def validate_integer(obj: Any, name: str = "object") -> None:
+  def _validate_integer(obj: Any, name: str = "object") -> None:
     """Ensure object is an integer."""
     if not isinstance(name, str):
       raise TypeError("Object name must be a string.")
@@ -27,9 +27,9 @@ class BaseRPC:
       raise TypeError(f"{name.capitalize()} must be an integer.")
 
   @staticmethod
-  def validate_integer_positive(obj: Any, name: str = "object") -> None:
+  def _validate_integer_positive(obj: Any, name: str = "object") -> None:
     """Ensure object is a positive integer."""
-    BaseRPC.validate_integer(obj, name)
+    BaseRPC._validate_integer(obj, name)
 
     if int(obj) < 0:
       raise ValueError(f"{name.capitalize()} must be greater or equal to zero.")
