a4c6a87e45e8a2a6859d52fd6921d3089fd6120c
Mahyar Mirrashed <mah.mirr@gmail.com>
2022-06-10 15:03:10 -0500

    feat: add final qualifier typing for type checkers

--- src/rpc/types/rpc.py
+++ src/rpc/types/rpc.py
@@ -1,16 +1,18 @@
 """Defines the types of RPCs outlined in the Raft book."""
 
+from typing_extensions import Final
+
 
 class RPC:
   # Chapter 3
-  APPEND_ENTRY = 1
-  REQUEST_VOTE = 2
+  APPEND_ENTRY: Final[int] = 1
+  REQUEST_VOTE: Final[int] = 2
   # Chapter 4
-  ADD_SERVER = 3
-  REMOVE_SERVER = 4
+  ADD_SERVER: Final[int] = 3
+  REMOVE_SERVER: Final[int] = 4
   # Chapter 5
-  INSTALL_SNAPSHOT = 5
+  INSTALL_SNAPSHOT: Final[int] = 5
   # Chapter 6
-  REGISTER_CLIENT = 6
-  CLIENT_REQUEST = 7
-  CLIENT_QUERY = 8
+  REGISTER_CLIENT: Final[int] = 6
+  CLIENT_REQUEST: Final[int] = 7
+  CLIENT_QUERY: Final[int] = 8
