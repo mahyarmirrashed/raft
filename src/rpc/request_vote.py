40008df396f8eec71ff3852ceaf96110d80eb9fc
Mahyar Mirrashed <mah.mirr@gmail.com>
2022-06-10 12:19:31 -0500

    feat(rpc): complete RequestVote rpc interface
    Completed both the request and response RequestVote classes.

--- src/rpc/request_vote.py
+++ src/rpc/request_vote.py
@@ -1,35 +1,25 @@
 """Defines the RequestVote RPC (Remote Procedure Call) as per Figure 3.1."""
 
+from db.objects import Identity
+from pydantic import NonNegativeInt, StrictBool
+from pydantic.dataclasses import dataclass
+
 from . import BaseRPCRequest, BaseRPCResponse
 
 
+@dataclass(frozen=True)
 class RequestVoteRPCRequest(BaseRPCRequest):
   """Implements RequestVote RPC request arguments."""
 
-  @property
-  def candidate_identity(self):
-    pass
-
-  @property
-  def last_log_index(self):
-    pass
-
-  @property
-  def last_log_term(self):
-    pass
-
-  @property
-  def term(self):
-    pass
+  term: NonNegativeInt
+  candidate_identity: Identity
+  last_log_index: NonNegativeInt
+  last_log_term: NonNegativeInt
 
 
+@dataclass(frozen=True)
 class RequestVoteRPCResponse(BaseRPCResponse):
   """Implements RequestVote RPC response results."""
 
-  @property
-  def vote_granted(self):
-    pass
-
-  @property
-  def term(self):
-    pass
+  term: NonNegativeInt
+  vote_granted: StrictBool
