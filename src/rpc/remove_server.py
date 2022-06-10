c88cd4c6e0ac3c82f42c3209333044eb2fd1a27c
Mahyar Mirrashed <mah.mirr@gmail.com>
2022-06-09 20:05:40 -0500

    feat(rpc): add RemoveServer rpc schema
    Implement the RemoveServer RPC, as defined in Figure 4.1 of Diego Ongaro's Raft book using classes.
    A separate class is created for the request and response.

---
+++ src/rpc/remove_server.py
@@ -0,0 +1,23 @@
+"""Defines the RemoveServer RPC (Remote Procedure Call) as per Figure 4.1."""
+
+from _base import BaseRPCRequest, BaseRPCResponse
+
+
+class RemoveServerRPCRequest(BaseRPCRequest):
+  """Implements RemoveServer RPC request arguments."""
+
+  @property
+  def old_server(self):
+    pass
+
+
+class RemoveServerRPCResponse(BaseRPCResponse):
+  """Implements RemoveServer RPC response results."""
+
+  @property
+  def status(self):
+    pass
+
+  @property
+  def leader_hint():
+    pass
