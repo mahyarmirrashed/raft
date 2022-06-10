7726d63cd4e41053fe4f4b7f06bdf6182a2a8063
Mahyar Mirrashed <mah.mirr@gmail.com>
2022-06-10 11:58:17 -0500

    refactor: update to use dataclasses instead
    Rather than implementing all the data validation, use Pydantic's built-in data validation via the
    dataclasses package.

--- src/db/objects/entry.py
+++ src/db/objects/entry.py
@@ -1,23 +1,11 @@
 """Defines a log entry."""
 
+from pydantic.dataclasses import dataclass
 
+
+@dataclass(frozen=True)
 class Entry:
   """Implements a log entry for the raft."""
 
-  def __init__(self, key: str, value: int) -> None:
-    # error checking
-    if not isinstance(key, str):
-      raise TypeError("Key must be a string.")
-    elif not isinstance(value, str):
-      raise TypeError("Value must be a string.")
-    # create object by assigning read-only parameters
-    self._key = key
-    self._value = value
-
-  @property
-  def key(self) -> str:
-    return self._key
-
-  @property
-  def value(self) -> int:
-    return self._value
+  key: str
+  value: str
