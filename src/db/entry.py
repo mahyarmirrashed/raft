b2a3e6d1f61c70713f841f55e9ca47602d8706c4
Mahyar Mirrashed <mah.mirr@gmail.com>
2022-06-11 17:03:37 -0500

    feat: add term to log entries
    Every log entry needs an associated term value to complete Raft's Log Matching property.

--- src/db/entry.py
+++ src/db/entry.py
@@ -1,10 +1,11 @@
 """Defines a log entry."""
 
-from pydantic import BaseModel, StrictStr
+from pydantic import BaseModel, NonNegativeInt, StrictStr
 
 
 class Entry(BaseModel):
   """Implements a log entry for the raft."""
 
+  term: NonNegativeInt
   key: StrictStr
   value: StrictStr
