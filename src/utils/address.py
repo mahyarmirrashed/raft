802a247b28d0de8a1e1c011fae4d15546392e280
Mahyar Mirrashed <mah.mirr@gmail.com>
2022-06-11 18:59:34 -0500

    fix: change type visibilities

--- src/utils/address.py
+++ src/utils/address.py
@@ -1,10 +1,8 @@
 """Defines an address."""
 
 import re
-from typing import Tuple
 
 from pydantic import BaseModel, ConstrainedInt, ConstrainedStr
-
 
 # https://stackoverflow.com/questions/106179/regular-expression-to-match-dns-hostname-or-ip-address
 REGEX_HOSTNAME = r"(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])"
@@ -13,15 +11,15 @@ REGEX_IP_ADDRESS = r"(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-
 REGEX_SOCKET_ADDRESS = f"^({REGEX_HOSTNAME}|{REGEX_IP_ADDRESS})$"
 
 
-class Host(ConstrainedStr):
+class _Host(ConstrainedStr):
   regex = re.compile(REGEX_SOCKET_ADDRESS)
 
 
-class Port(ConstrainedInt):
+class _Port(ConstrainedInt):
   ge = 0
   lt = 65536
 
 
 class Address(BaseModel):
-  host: Host
-  port: Port
+  host: _Host
+  port: _Port
