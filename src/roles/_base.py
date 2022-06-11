c4c45bf5fe40b08cd46a4ea09633abe07e00015c
Mahyar Mirrashed <mah.mirr@gmail.com>
2022-06-11 00:44:59 -0500

    fix: account for possibly null voted_for
    We could have not voted in this current term in which case the voted_for attribute would be None.

--- src/roles/_base.py
+++ src/roles/_base.py
@@ -1,6 +1,6 @@
 """Defines the base server role."""
 
-from typing import List
+from typing import List, Union
 
 from db import Entry
 from pydantic import BaseModel, NonNegativeInt
@@ -11,7 +11,7 @@ class BaseRole(BaseModel):
   """Implements state properties present on all servers."""
 
   current_term: NonNegativeInt
-  voted_for: Address
+  voted_for: Union[Address, None]
   log: List[Entry]
   commit_index: NonNegativeInt
   last_applied_index: NonNegativeInt
